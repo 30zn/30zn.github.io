97a3a6701ab9e50791594ee901590acbce48ae41
30zn <124384963+30zn@users.noreply.github.com>
2026-09-21 19:06:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function discordMain() {
                 )
 
                 // 이름설정
-                profileName.textContent = "user.username"
+                profileName.textContent = user.username
                     //user.username로 작성시 디스코드 아이디가 표시됨
 
                 // 클립보드에 이름 복사
